411b6953e0b6b88fea81b0e04608eb02fe45565f
cloner01010 <ali.zamani@alumni.fh-aachen.de>
2023-05-30 20:18:58 +0200

    icons changing from font awesome to bootstrap

--- resources/views/newsite.blade.php
+++ resources/views/newsite.blade.php
@@ -6,9 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Document</title>
+    <title>Neuer Einstieg</title>
     <script src="{{asset('storage/js/newsite.js')}}" defer></script>
-    <script src="https://kit.fontawesome.com/87db36f807.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{asset('storage/css/newsite.css')}}">
 </head>
 <body>
