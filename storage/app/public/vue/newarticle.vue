080f0923f073a4efd6425c8920503023a0dc0499
AyAnniix248 <yannik.niewoehner@gmail.com>
2023-06-08 13:39:49 +0200

    task 2 - 4 finished (sass added)

---
+++ storage/app/public/vue/newarticle.vue
@@ -0,0 +1,24 @@
+<template lang="">
+<div id="session_creator" data-creator="{{ session('abalo_user') }}"></div>
+<div id="newarticle-main-container">
+    <script src="{{ asset('storage/js/articles.js') }}"></script>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
